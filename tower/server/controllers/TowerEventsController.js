4cbc3c1f2a3886ba24a5ed294f9203854d599d9c
Annika <nikzneko@gmail.com>
2022-06-22 16:26:44 -0600

    FINALLY! I actually have a functional method!!!! After like 5 hours I'm finally at the START of my project

--- tower/server/controllers/TowerEventsController.js
+++ tower/server/controllers/TowerEventsController.js
@@ -1,5 +1,6 @@
 import { towerEventsService } from "../services/TowerEventsService"
-// import { Auth0Provider } from '@bcwdev/auth0provider'
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { TowerEventSchema } from "../models/TowerEvent"
 import BaseController from "../utils/BaseController"
 
 
@@ -8,15 +9,27 @@ import BaseController from "../utils/BaseController"
 
 export class TowerEventsController extends BaseController {
   constructor() {
-    super('api/towerevents')
+    super('api/events')
     this.router
       // .gets
-      // .use
+      .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
   }
 
+  // mobs code
+  // async create(req, res, next) {
+  //   try {
+  //     req.body.creatorId = req.userInfo.id
+  //     const project = await projectsService.create(req.body)
+  //     return res.send(project)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
   async create(req, res, next) {
     try {
+      req.body.creatorId = req.userInfo.id
       const towerEvent = await towerEventsService.create(req.body)
       return res.send(towerEvent)
     } catch (error) {
--- tower/server/services/TowerEventsService.js
+++ tower/server/services/TowerEventsService.js
@@ -2,13 +2,19 @@ import { dbContext } from "../db/DbContext"
 import { BadRequest } from "../utils/Errors"
 
 
-
+// Mobs code
+// async create(body) {
+//   let project = await dbContext.Projects.create(body)
+//   await project.populate('creator', 'name picture')
+//   return project
+// }
 
 
 class TowerEventsService {
   async create(body) {
     let towerEvent = await dbContext.TowerEvents.create(body)
     // populate creator here
+    return towerEvent
   }
 }
 
